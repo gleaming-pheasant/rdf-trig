fbc4bef246f2814d05e75ee214efa6dcd961aab8
gleaming-pheasant <patrick.mchugh@york.ac.uk>
2026-03-09 09:00:15 +0000

    Add further static predicates

--- src/namespaces/predicates.rs
+++ src/namespaces/predicates.rs
@@ -3,9 +3,19 @@
 use crate::namespaces::statics::{AOCAT, RDF, RDFS, SKOS};
 use crate::nodes::Predicate;
 
+/// aocat:from
+pub const AOCAT_FROM: Predicate = Predicate::new_const(
+    AOCAT, "from"
+);
+
 /// aocat:has_ARIADNE_subject
 pub const AOCAT_ARIADNE_SUBJECT: Predicate = Predicate::new_const(
     AOCAT, "has_ARIADNE_subject"
+);
+
+/// aocat:has_access_policy
+pub const AOCAT_ACCESS_POLICY: Predicate = Predicate::new_const(
+    AOCAT, "has_access_policy"
 );
 
 /// aocat:has_access_rights
@@ -13,9 +23,34 @@ pub const AOCAT_ACCESS_RIGHTS: Predicate = Predicate::new_const(
     AOCAT, "has_access_rights"
 );
 
+/// aocat:has_bounding_box_max_lon
+pub const AOCAT_BB_MAX_X: Predicate = Predicate::new_const(
+    AOCAT, "has_bounding_box_max_lon"
+);
+
+/// aocat:has_bounding_box_max_lat
+pub const AOCAT_BB_MAX_Y: Predicate = Predicate::new_const(
+    AOCAT, "has_bounding_box_max_lat"
+);
+
+/// aocat:has_bounding_box_min_lon
+pub const AOCAT_BB_MIN_X: Predicate = Predicate::new_const(
+    AOCAT, "has_bounding_box_min_lon"
+);
+
+/// aocat:has_bounding_box_min_lat
+pub const AOCAT_BB_MIN_Y: Predicate = Predicate::new_const(
+    AOCAT, "has_bounding_box_min_lat"
+);
+
 /// aocat:has_contributor
 pub const AOCAT_CONTRIBUTOR: Predicate = Predicate::new_const(
     AOCAT, "has_contributor"
+);
+
+/// aocat:has_country
+pub const AOCAT_COUNTRY: Predicate = Predicate::new_const(
+    AOCAT, "has_country"
 );
 
 /// aocat:has_creator
@@ -38,6 +73,11 @@ pub const AOCAT_DESCRIPTION: Predicate = Predicate::new_const(
     AOCAT, "has_description"
 );
 
+/// aocat:has_identifier
+pub const AOCAT_IDENTIFIER: Predicate = Predicate::new_const(
+    AOCAT, "has_identifier"
+);
+
 /// aocat:has_landing_page
 pub const AOCAT_LANDING_PAGE: Predicate = Predicate::new_const(
     AOCAT, "has_landing_page"
@@ -46,6 +86,16 @@ pub const AOCAT_LANDING_PAGE: Predicate = Predicate::new_const(
 /// aocat:has_language
 pub const AOCAT_LANGUAGE: Predicate = Predicate::new_const(
     AOCAT, "has_language"
+);
+
+/// aocat:has_latitude
+pub const AOCAT_LATITUDE: Predicate = Predicate::new_const(
+    AOCAT, "has_latitude"
+);
+
+/// aocat:has_longitude
+pub const AOCAT_LONGITUDE: Predicate = Predicate::new_const(
+    AOCAT, "has_longitude"
 );
 
 /// aocat:has_native_subject
@@ -63,6 +113,26 @@ pub const AOCAT_OWNER: Predicate = Predicate::new_const(
     AOCAT, "has_owner"
 );
 
+/// aocat:has_period
+pub const AOCAT_PERIOD: Predicate = Predicate::new_const(
+    AOCAT, "has_period"
+);
+
+/// aocat:has_place_name
+pub const AOCAT_PLACE_NAME: Predicate = Predicate::new_const(
+    AOCAT, "has_place_name"
+);
+
+/// aocat:has_polygonal_representation
+pub const AOCAT_POLYGONAL: Predicate = Predicate::new_const(
+    AOCAT, "has_polygonal_representation"
+);
+
+/// aocat:has_native_period
+pub const AOCAT_NATIVE_PERIOD: Predicate = Predicate::new_const(
+    AOCAT, "has_native_period"
+);
+
 /// aocat:has_publisher
 pub const AOCAT_PUBLISHER: Predicate = Predicate::new_const(
     AOCAT, "has_publisher"
@@ -71,6 +141,61 @@ pub const AOCAT_PUBLISHER: Predicate = Predicate::new_const(
 /// aocat:has_responsible
 pub const AOCAT_RESPONSIBLE: Predicate = Predicate::new_const(
     AOCAT, "has_responsible"
+);
+
+/// aocat:has_spatial_coverage
+pub const AOCAT_SPATIAL_COVERAGE: Predicate = Predicate::new_const(
+    AOCAT, "has_spatial_coverage"
+);
+
+/// aocat:has_temporal_coverage
+pub const AOCAT_TEMPORAL_COVERAGE: Predicate = Predicate::new_const(
+    AOCAT, "has_temporal_coverage"
+);
+
+/// aocat:has_title
+pub const AOCAT_TITLE: Predicate = Predicate::new_const(
+    AOCAT, "has_title"
+);
+
+/// aocat:has_type
+pub const AOCAT_TYPE: Predicate = Predicate::new_const(
+    AOCAT, "has_type"
+);
+
+/// aocat:has_visual_component
+pub const AOCAT_VISUAL_COMPONENT: Predicate = Predicate::new_const(
+    AOCAT, "has_visual_component"
+);
+
+/// aocat:is_rendered_by
+pub const AOCAT_RENDERED_BY: Predicate = Predicate::new_const(
+    AOCAT, "is_rendered_by"
+);
+
+/// aocat:until
+pub const AOCAT_UNTIL: Predicate = Predicate::new_const(
+    AOCAT, "until"
+);
+
+/// aocat:was_issued
+pub const AOCAT_ISSUED: Predicate = Predicate::new_const(
+    AOCAT, "was_issued"
+);
+
+/// aocat:was_modified
+pub const AOCAT_MODIFIED: Predicate = Predicate::new_const(
+    AOCAT, "was_modified"
+);
+
+/// aocat:was_created_on
+pub const AOCAT_CREATED_ON: Predicate = Predicate::new_const(
+    AOCAT, "was_created_on"
+);
+
+/// aocat:has_primary_visual_component
+pub const AOCAT_PRIMARY_VISUAL_COMPONENT: Predicate = Predicate::new_const(
+    AOCAT, "has_primary_visual_component"
 );
 
 /// rdf:type
