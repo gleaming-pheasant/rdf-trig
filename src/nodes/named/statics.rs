73dd65674b65ef1d4abc899e4295f516e5deec6e
gleaming-pheasant <patrick.mchugh@york.ac.uk>
2026-04-01 10:44:25 +0100

    Add aocat:has_spatial_coverage

--- src/nodes/named/statics.rs
+++ src/nodes/named/statics.rs
@@ -114,6 +114,7 @@ macro_rules! define_ontology {
 
 define_ontology!(aocat, "https://www.ariadne-infrastructure.eu/resource/ao/cat/1.1/",
     Properties {
+        HasSpatialCoverage => "has_spatial_coverage",
         HasIdentifier => "has_identifier",
         HasType => "has_type",
         IsTypeOf => "is_type_of",
