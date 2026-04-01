6fc6ed14c853a017a533e52522d147b79a7be9b7
gleaming-pheasant <patrick.mchugh@york.ac.uk>
2026-04-01 11:07:30 +0100

    Add aocat:has_country propert

--- src/nodes/named/statics.rs
+++ src/nodes/named/statics.rs
@@ -114,6 +114,7 @@ macro_rules! define_ontology {
 
 define_ontology!(aocat, "https://www.ariadne-infrastructure.eu/resource/ao/cat/1.1/",
     Properties {
+        HasCountry => "has_country",
         HasSpatialCoverage => "has_spatial_coverage",
         HasIdentifier => "has_identifier",
         HasType => "has_type",
